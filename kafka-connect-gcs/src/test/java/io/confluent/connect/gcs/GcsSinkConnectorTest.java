a7e436071c7c439808f0cc901651ea8765540a3c
Raphael Hamonnais <raphael.hamonnais@datadoghq.com>
2019-01-28 16:24:38 +0100

    Add GcsSinkConnectorTest

---
+++ kafka-connect-gcs/src/test/java/io/confluent/connect/gcs/GcsSinkConnectorTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2018 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License; you may not use this file
+ * except in compliance with the License.  You may obtain a copy of the License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package io.confluent.connect.gcs;
+
+import org.apache.kafka.connect.connector.Connector;
+import org.apache.kafka.connect.sink.SinkConnector;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GcsSinkConnectorTest {
+
+  @Test
+  public void testVersion() {
+    String version = new GcsSinkConnector().version();
+    assertNotNull(version);
+    assertFalse(version.isEmpty());
+  }
+
+  @Test
+  public void connectorType() {
+    Connector connector = new GcsSinkConnector();
+    assertTrue(SinkConnector.class.isAssignableFrom(connector.getClass()));
+  }
+}
+
